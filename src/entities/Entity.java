789295ab059b98c74564fca0b4f2376aabfa69d6
JamesSmith05 <JS211384@reigate.ac.uk>
2022-03-28 16:20:11 +0100

    -

--- src/entities/Entity.java
+++ src/entities/Entity.java
@@ -10,7 +10,7 @@ import java.awt.image.BufferedImage;
 public class Entity {
 
     GamePanel gp;
-    public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2, stationary;
+    public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2;
     public BufferedImage attackUp1, attackUp2, attackDown1, attackDown2, attackLeft1, attackLeft2, attackRight1, attackRight2;
     public BufferedImage image, image2, image3;
     public Rectangle solidArea = new Rectangle(0, 0, 48, 48);
